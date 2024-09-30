fed1a3a44e8f5232b982a62bc12a020fd02b929a
Liam Horan-Portelance <142847439+lhoranportelance@users.noreply.github.com>
2024-09-30 09:55:20 -0400

    Create 3_create_output_directories.sh

---
+++ quants_demux_doublets/3_create_output_directories.sh
@@ -0,0 +1,16 @@
+# run in command-line
+
+FASTQ_LIST=$(cat /data/ADRD/amp_pd/transcriptomics/fastq_processing/cellranger/all_fastqs.txt)
+
+for FASTQ in ${FASTQ_LIST}
+do
+mkdir /data/ADRD/amp_pd/transcriptomics/fastq_processing/demuxafy/samples_outs/$FASTQ/doubletdetection
+mkdir /data/ADRD/amp_pd/transcriptomics/fastq_processing/demuxafy/samples_outs/$FASTQ/scdblfinder
+mkdir /data/ADRD/amp_pd/transcriptomics/fastq_processing/demuxafy/samples_outs/$FASTQ/scds
+mkdir /data/ADRD/amp_pd/transcriptomics/fastq_processing/demuxafy/samples_outs/$FASTQ/demuxalot
+mkdir /data/ADRD/amp_pd/transcriptomics/fastq_processing/demuxafy/samples_outs/$FASTQ/demuxlet
+mkdir /data/ADRD/amp_pd/transcriptomics/fastq_processing/demuxafy/samples_outs/$FASTQ/vireo
+mkdir /data/ADRD/amp_pd/transcriptomics/fastq_processing/demuxafy/samples_outs/$FASTQ/combined
+mkdir /data/ADRD/amp_pd/transcriptomics/fastq_processing/cellbender/outs/$FASTQ
+mkdir /data/ADRD/amp_pd/transcriptomics/fastq_processing/final_outs/$FASTQ
+done
