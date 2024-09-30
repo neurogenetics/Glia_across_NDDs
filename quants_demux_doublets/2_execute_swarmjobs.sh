3e1642a8fe8dbf9149ae409dca469e3c23ed4f77
Liam Horan-Portelance <142847439+lhoranportelance@users.noreply.github.com>
2024-09-30 10:28:38 -0400

    Update 2_execute_swarmjobs.sh

--- quants_demux_doublets/2_execute_swarmjobs.sh
+++ quants_demux_doublets/2_execute_swarmjobs.sh
@@ -7,7 +7,7 @@ swarm -f /data/ADRD/amp_pd/transcriptomics/fastq_processing/swarmscripts/cellran
     --module cellranger \
     --logdir /data/ADRD/amp_pd/transcriptomics/fastq_processing/swarmlogs \
     --time=36:00:00 \
-    --job-name swarm_cellranger_test
+    --job-name swarm_cellranger_amppd
 # cellbender
 swarm -f /data/ADRD/amp_pd/transcriptomics/fastq_processing/swarmscripts/cellbender_swarm.swarm \
     -g 100 \
@@ -17,7 +17,7 @@ swarm -f /data/ADRD/amp_pd/transcriptomics/fastq_processing/swarmscripts/cellben
     --time=24:00:00 \
     --partition=gpu \
     --gres=gpu:k80:1 \
-    --job-name swarm_cellbender_test
+    --job-name swarm_cellbender_amppd
 # doubletdetection
 swarm -f /data/ADRD/amp_pd/transcriptomics/fastq_processing/swarmscripts/doubletdetection_swarm.swarm \
     -g 50 \
@@ -25,7 +25,7 @@ swarm -f /data/ADRD/amp_pd/transcriptomics/fastq_processing/swarmscripts/doublet
     --module singularity \
     --logdir /data/ADRD/amp_pd/transcriptomics/fastq_processing/swarmlogs \
     --time=24:00:00 \
-    --job-name swarm_doubletdetection_test
+    --job-name swarm_doubletdetection_amppd
 # scdblfinder
 swarm -f /data/ADRD/amp_pd/transcriptomics/fastq_processing/swarmscripts/scdblfinder_swarm.swarm \
     -g 50 \
@@ -33,7 +33,7 @@ swarm -f /data/ADRD/amp_pd/transcriptomics/fastq_processing/swarmscripts/scdblfi
     --module singularity \
     --logdir /data/ADRD/amp_pd/transcriptomics/fastq_processing/swarmlogs \
     --time=24:00:00 \
-    --job-name swarm_scdblfinder_test
+    --job-name swarm_scdblfinder_amppd
 # scds
 swarm -f /data/ADRD/amp_pd/transcriptomics/fastq_processing/swarmscripts/scds_swarm.swarm \
     -g 50 \
@@ -41,7 +41,7 @@ swarm -f /data/ADRD/amp_pd/transcriptomics/fastq_processing/swarmscripts/scds_sw
     --module singularity \
     --logdir /data/ADRD/amp_pd/transcriptomics/fastq_processing/swarmlogs \
     --time=24:00:00 \
-    --job-name swarm_scds_test
+    --job-name swarm_scds_amppd
 # demuxalot
 swarm -f /data/ADRD/amp_pd/transcriptomics/fastq_processing/swarmscripts/demuxalot_swarm.swarm \
     -g 100 \
@@ -49,7 +49,7 @@ swarm -f /data/ADRD/amp_pd/transcriptomics/fastq_processing/swarmscripts/demuxal
     --module singularity,bcftools,samtools \
     --logdir /data/ADRD/amp_pd/transcriptomics/fastq_processing/swarmlogs \
     --time=24:00:00 \
-    --job-name swarm_demuxalot_test
+    --job-name swarm_demuxalot_amppd
 # vireo
 swarm -f /data/ADRD/amp_pd/transcriptomics/fastq_processing/swarmscripts/vireo_swarm.swarm \
     -g 20 \
@@ -57,7 +57,7 @@ swarm -f /data/ADRD/amp_pd/transcriptomics/fastq_processing/swarmscripts/vireo_s
     --module singularity \
     --logdir /data/ADRD/amp_pd/transcriptomics/fastq_processing/swarmlogs \
     --time=48:00:00 \
-    --job-name swarm_vireo_test
+    --job-name swarm_vireo_amppd
 # combine results
 swarm -f /data/ADRD/amp_pd/transcriptomics/fastq_processing/swarmscripts/demuxafy_combine_results.swarm \
     -g 3 \
@@ -65,7 +65,7 @@ swarm -f /data/ADRD/amp_pd/transcriptomics/fastq_processing/swarmscripts/demuxaf
     --module singularity \
     --logdir /data/ADRD/amp_pd/transcriptomics/fastq_processing/swarmlogs \
     --time=00:10:00 \
-    --job-name swarm_combine_results_test
+    --job-name swarm_combine_results_amppd
 # filter doublets
 swarm -f /data/ADRD/amp_pd/transcriptomics/fastq_processing/swarmscripts/filter_doublets.swarm \
     -g 50 \
@@ -73,5 +73,5 @@ swarm -f /data/ADRD/amp_pd/transcriptomics/fastq_processing/swarmscripts/filter_
     --module R \
     --logdir /data/ADRD/amp_pd/transcriptomics/fastq_processing/swarmlogs \
     --time=2:00:00 \
-    --job-name swarm_filter_cells_test \
+    --job-name swarm_filter_cells_amppd \
     --gres=lscratch:5
