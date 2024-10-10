b1e23cb56c8c4b218b1b2f575cca13a3bb7f3942
Liam Horan-Portelance <142847439+lhoranportelance@users.noreply.github.com>
2024-10-10 11:41:57 -0400

    Update create_swarmscripts.sh

--- quants_demux_doublets/ALS_FTD_multiregion/create_swarmscripts.sh
+++ quants_demux_doublets/ALS_FTD_multiregion/create_swarmscripts.sh
@@ -1,1 +1,8 @@
+SWARM_FILENAME=/data/ADRD/ALSFTD_multiregion/fastq_processing/swarmscripts/prefetch_fasterq_dump_swarm.swarm
+FASTQ_LIST=$(cat /data/ADRD/ALSFTD_multiregion/fastq_processing/all_fastqs.txt)
+JOB_SCRIPT=/data/ADRD/ALSFTD_multiregion/fastq_processing/jobscripts/prefetch_fasterq_dump_swarm.sh
 
+for FASTQ in ${FASTQ_LIST}
+do
+echo ${JOB_SCRIPT} ${FASTQ} >> ${SWARM_FILENAME}
+done
