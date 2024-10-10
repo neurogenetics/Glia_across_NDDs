6c9c69fcc70070a8dc4b1ad67efc7c6fbba63187
Liam Horan-Portelance <142847439+lhoranportelance@users.noreply.github.com>
2024-10-10 11:56:15 -0400

    Update execute_swarmjobs.sh

--- quants_demux_doublets/ALS_FTD_multiregion/execute_swarmjobs.sh
+++ quants_demux_doublets/ALS_FTD_multiregion/execute_swarmjobs.sh
@@ -1,1 +1,10 @@
-
+# sratoolkit (prefetch + fasterq-dump)
+swarm -f /data/ADRD/ALSFTD_multiregion/fastq_processing/swarmscripts/prefetch_fasterq_dump_swarm.swarm \
+    -g 10 \
+    -t 10 \
+    -b 20 \
+    --module sratoolkit \
+    --logdir /data/ADRD/ALSFTD_multiregion/fastq_processing/swarmlogs \
+    --time=36:00:00 \
+    --gres=lscratch:500 \
+    --job-name swarm_sra_alsftd
