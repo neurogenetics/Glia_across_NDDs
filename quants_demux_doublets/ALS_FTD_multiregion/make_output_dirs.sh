3972f5d87aa7c2581c15aa2965d1e81861c45c22
Liam Horan-Portelance <142847439+lhoranportelance@users.noreply.github.com>
2024-10-10 11:37:07 -0400

    Update make_output_dirs.sh

--- quants_demux_doublets/ALS_FTD_multiregion/make_output_dirs.sh
+++ quants_demux_doublets/ALS_FTD_multiregion/make_output_dirs.sh
@@ -1,4 +1,4 @@
-FASTQ_LIST=$(cat /data/ADRD/ALSFTD_multiregion/fastq_processing/all_fastqs/ALS_SRRs.txt)
+FASTQ_LIST=$(cat /data/ADRD/ALSFTD_multiregion/fastq_processing/all_fastqs/cleaned_SRRs.txt)
 
 for FASTQ in ${FASTQ_LIST}
 do
