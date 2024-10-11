ede728dd9667728fbfd61e2af48f49fd3d08dfe4
Liam Horan-Portelance <142847439+lhoranportelance@users.noreply.github.com>
2024-10-11 13:57:59 -0400

    Update organize_fastqs.sh

--- quants_demux_doublets/AD_multiregion/organize_fastqs.sh
+++ quants_demux_doublets/AD_multiregion/organize_fastqs.sh
@@ -3,6 +3,6 @@ FASTQ_LIST=$(cat /data/ADRD/AD_multiregion/fastq_processing/cleaned_AD_fastqs.tx
 for FASTQ in ${FASTQ_LIST}
 do
 mkdir -p /data/ADRD/AD_multiregion/fastq_processing/all_fastqs/$FASTQ
-mv /data/ADRD/AD_multiregion/fastqs/'Sequencing Round 1'/*$FASTQ /data/ADRD/AD_multiregion/fastq_processing/all_fastqs/$FASTQ/
-mv /data/ADRD/AD_multiregion/fastqs/'Sequencing Round 2'/*$FASTQ /data/ADRD/AD_multiregion/fastq_processing/all_fastqs/$FASTQ/
+mv /data/ADRD/AD_multiregion/fastqs/'Sequencing Round 1'/$FASTQ* /data/ADRD/AD_multiregion/fastq_processing/all_fastqs/$FASTQ/
+mv /data/ADRD/AD_multiregion/fastqs/'Sequencing Round 2'/$FASTQ* /data/ADRD/AD_multiregion/fastq_processing/all_fastqs/$FASTQ/
 done
