3e6b65cc30c7aadd0c4baba572347b8b1a1bf74b
Liam Horan-Portelance <142847439+lhoranportelance@users.noreply.github.com>
2024-10-15 15:20:11 -0400

    Update rename_fastqs.sh

--- quants_demux_doublets/ALS_FTD_multiregion/rename_fastqs.sh
+++ quants_demux_doublets/ALS_FTD_multiregion/rename_fastqs.sh
@@ -1,6 +1,6 @@
 FASTQ_LIST=$(cat /data/ADRD/ALSFTD_multiregion/fastq_processing/cleaned_SRRs.txt)
 
-for FASTQ in FASTQ_LIST
+for FASTQ in ${FASTQ_LIST}
 do
 FASTQ_DIR=/data/ADRD/ALSFTD_multiregion/fastq_processing/all_fastqs/fastq/$FASTQ
 mv ${FASTQ_DIR}/${FASTQ}_1.fastq.gz ${FASTQ_DIR}/${FASTQ}_S1_R1_001.fastq.gz
