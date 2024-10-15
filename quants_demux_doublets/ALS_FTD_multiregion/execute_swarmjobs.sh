1fd8da03b33541e01aa3c18ad6ecae70a8995992
Liam Horan-Portelance <142847439+lhoranportelance@users.noreply.github.com>
2024-10-15 16:44:42 -0400

    Update execute_swarmjobs.sh

--- quants_demux_doublets/ALS_FTD_multiregion/execute_swarmjobs.sh
+++ quants_demux_doublets/ALS_FTD_multiregion/execute_swarmjobs.sh
@@ -8,3 +8,14 @@ swarm -f /data/ADRD/ALSFTD_multiregion/fastq_processing/swarmscripts/prefetch_fa
     --time=12:00:00 \
     --gres=lscratch:500 \
     --job-name swarm_sra_alsftd
+
+# cellranger
+swarm -f /data/ADRD/ALSFTD_multiregion/fastq_processing/swarmscripts/cellranger_swarm.swarm \
+    -g 124 \
+    -t 24 \
+    -b 3 \
+    --module cellranger \
+    --logdir /data/ADRD/ALSFTD_multiregion/fastq_processing/swarmlogs \
+    --time=18:00:00 \
+    --gres=lscratch:700 \
+    --job-name swarm_cellranger_alsftd
