b0646ace8afa1ec6487f71517ecec61513fe5991
Liam Horan-Portelance <142847439+lhoranportelance@users.noreply.github.com>
2024-10-17 08:07:14 -0400

    Create prefetch_fasterq_dump.sh

---
+++ quants_demux_doublets/ALS_FTD_multiregion/prefetch_fasterq_dump.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+SRR=${1}
+PREFETCH_OUTDIR=/data/ADRD/ALSFTD_multiregion/fastq_processing/all_fastqs/sra
+DOWNLOAD_OUTDIR=/data/ADRD/ALSFTD_multiregion/fastq_processing/all_fastqs/fastq/$SRR
+TMPDIR=/lscratch/$SLURM_JOBID
+
+module load sratoolkit
+
+prefetch $SRR \
+    -O $PREFETCH_OUTDIR \
+    --max-size 45g \
+    --force all
+fasterq-dump $PREFETCH_OUTDIR/${SRR}/${SRR}.sra \
+    -t $TMPDIR \
+    -O $DOWNLOAD_OUTDIR \
+    --skip-technical
+gzip $DOWNLOAD_OUTDIR/*.fastq
+find $DOWNLOAD_OUTDIR -type f -name "*.fastq" -not -name "*.fastq.gz" -delete
+rm -r $PREFETCH_OUTDIR/${SRR}
+rm -r $TMPDIR/*
