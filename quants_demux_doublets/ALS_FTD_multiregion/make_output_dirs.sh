35a69517b9d54d4fdf55df54ad6b2821ce3cec50
Liam Horan-Portelance <142847439+lhoranportelance@users.noreply.github.com>
2024-10-17 09:36:09 -0400

    Update make_output_dirs.sh

--- quants_demux_doublets/ALS_FTD_multiregion/make_output_dirs.sh
+++ quants_demux_doublets/ALS_FTD_multiregion/make_output_dirs.sh
@@ -5,4 +5,7 @@ do
 mkdir /data/ADRD/ALSFTD_multiregion/fastq_processing/all_fastqs/fastq/$FASTQ
 mkdir /data/ADRD/ALSFTD_multiregion/fastq_processing/cellranger/outs/$FASTQ
 mkdir /data/ADRD/ALSFTD_multiregion/fastq_processing/cellbender/outs/$FASTQ
+mkdir /data/ADRD/ALSFTD_multiregion/fastq_processing/demuxafy/outs/$FASTQ/doubletdetection
+mkdir /data/ADRD/ALSFTD_multiregion/fastq_processing/demuxafy/outs/$FASTQ/scdblfinder
+mkdir /data/ADRD/ALSFTD_multiregion/fastq_processing/demuxafy/outs/$FASTQ/scds
 done
