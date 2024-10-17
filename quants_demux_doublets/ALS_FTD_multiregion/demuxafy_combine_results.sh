c045034d5d07da23154f34a7ba7ccef64bb0238c
Liam Horan-Portelance <142847439+lhoranportelance@users.noreply.github.com>
2024-10-17 18:00:57 -0400

    Create demuxafy_combine_results.sh

---
+++ quants_demux_doublets/ALS_FTD_multiregion/demuxafy_combine_results.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+FASTQ=${1}
+
+OUTDIR=/data/ADRD/ALSFTD_multiregion/fastq_processing/demuxafy/outs/$FASTQ/combined
+DOUBLETDETECTION_OUTDIR=/data/ADRD/ALSFTD_multiregion/fastq_processing/demuxafy/outs/$FASTQ/doubletdetection
+SCDBLFINDER_OUTDIR=/data/ADRD/ALSFTD_multiregion/fastq_processing/demuxafy/outs/$FASTQ/scdblfinder
+SCDS_OUTDIR=/data/ADRD/ALSFTD_multiregion/fastq_processing/demuxafy/outs/$FASTQ/scds
+
+module load singularity
+
+export SINGULARITY_CACHEDIR=/data/ADRD/ALSFTD_multiregion/fastq_processing/demuxafy/.singularity
+
+singularity exec --bind /data/ADRD/ALSFTD_multiregion/fastq_processing Demuxafy.sif Combine_Results.R \
+  -o $OUTDIR/combined_results.tsv \
+  -t $DOUBLETDETECTION_OUTDIR \
+  -n $SCDBLFINDER_OUTDIR \
+  -c $SCDS_OUTDIR 
