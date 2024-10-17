cfdfd293c6879c88e9903908e1f95b59f8d889de
Liam Horan-Portelance <142847439+lhoranportelance@users.noreply.github.com>
2024-10-17 09:47:28 -0400

    Create run_demuxafy_doubletdetection_swarm.sh

---
+++ quants_demux_doublets/ALS_FTD_multiregion/run_demuxafy_doubletdetection_swarm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+FASTQ=${1}
+COUNTS=/data/ADRD/ALSFTD_multiregion/fastq_processing/cellranger/outs/$FASTQ/filtered_feature_bc_matrix
+BARCODES=/data/ADRD/ALSFTD_multiregion/fastq_processing/cellranger/outs/$FASTQ/filtered_feature_bc_matrix/barcodes.tsv
+DOUBLETDETECTION_OUTDIR=/data/ADRD/ALSFTD_multiregion/fastq_processing/demuxafy/outs/$FASTQ/doubletdetection
+
+module load singularity
+
+export SINGULARITY_CACHEDIR=/data/ADRD/ALSFTD_multiregion/fastq_processing/demuxafy/.singularity
+
+singularity exec --bind /data/ADRD/ALSFTD_multiregion/fastq_processing Demuxafy.sif DoubletDetection.py \
+    -m $COUNTS \
+    -b $BARCODES \
+    -o $DOUBLETDETECTION_OUTDIR 
