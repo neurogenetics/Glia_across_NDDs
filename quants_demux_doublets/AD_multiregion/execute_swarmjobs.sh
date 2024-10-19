97c55a959634d5bb498caec4830802bcef1cd5b6
Liam Horan-Portelance <142847439+lhoranportelance@users.noreply.github.com>
2024-10-19 16:50:39 -0400

    Create execute_swarmjobs.sh

---
+++ quants_demux_doublets/AD_multiregion/execute_swarmjobs.sh
@@ -0,0 +1,9 @@
+# cellranger
+swarm -f /data/ADRD/AD_multiregion/fastq_processing/swarmscripts/cellranger_swarm.swarm \
+    -g 124 \
+    -t 24 \
+    --module cellranger \
+    --logdir /data/ADRD/AD_multiregion/fastq_processing/swarmlogs \
+    --time=18:00:00 \
+    --gres=lscratch:700 \
+    --job-name swarm_cellranger_ad
