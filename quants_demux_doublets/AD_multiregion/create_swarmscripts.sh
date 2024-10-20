23900ee67935f0d7b300338ba683fd0f60cca7e2
Liam Horan-Portelance <142847439+lhoranportelance@users.noreply.github.com>
2024-10-20 10:26:28 -0400

    Update create_swarmscripts.sh

--- quants_demux_doublets/AD_multiregion/create_swarmscripts.sh
+++ quants_demux_doublets/AD_multiregion/create_swarmscripts.sh
@@ -8,3 +8,14 @@ echo ${JOB_SCRIPT} ${FASTQ} >> ${SWARM_FILENAME}
 done
 
 ##########
+
+SWARM_FILENAME=/data/ADRD/AD_multiregion/fastq_processing/swarmscripts/cellranger_swarm.swarm
+FASTQ_LIST=$(cat /data/ADRD/AD_multiregion/fastq_processing/round_1_fastqs.txt)
+JOB_SCRIPT=/data/ADRD/AD_multiregion/fastq_processing/jobscripts/run_cellranger_swarm.sh
+
+for FASTQ in ${FASTQ_LIST}
+do
+echo ${JOB_SCRIPT} ${FASTQ} >> ${SWARM_FILENAME}
+done
+
+##########
