a5f4453e84fd85017a3d72c994a0ecd02653808c
Liam Horan-Portelance <142847439+lhoranportelance@users.noreply.github.com>
2024-10-20 10:28:37 -0400

    Update execute_swarmjobs.sh

--- quants_demux_doublets/AD_multiregion/execute_swarmjobs.sh
+++ quants_demux_doublets/AD_multiregion/execute_swarmjobs.sh
@@ -7,3 +7,15 @@ swarm -f /data/ADRD/AD_multiregion/fastq_processing/swarmscripts/cellranger_swar
     --time=18:00:00 \
     --gres=lscratch:700 \
     --job-name swarm_cellranger_ad
+
+# cellbender
+swarm -f /data/ADRD/AD_multiregion/fastq_processing/swarmscripts/cellbender_swarm.swarm \
+    -g 100 \
+    -t 12 \
+    -b 2 \
+    --module cellbender \
+    --logdir /data/ADRD/AD_multiregion/fastq_processing/swarmlogs \
+    --time=12:00:00 \
+    --partition=gpu \
+    --gres=gpu:k80:1 \
+    --job-name swarm_cellbender_alsftd
