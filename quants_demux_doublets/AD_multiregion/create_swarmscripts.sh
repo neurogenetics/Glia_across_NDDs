60455ec875c1f751f0770b431ebec3d2fb96cc02
Liam Horan-Portelance <142847439+lhoranportelance@users.noreply.github.com>
2024-10-20 13:46:03 -0400

    Update create_swarmscripts.sh

--- quants_demux_doublets/AD_multiregion/create_swarmscripts.sh
+++ quants_demux_doublets/AD_multiregion/create_swarmscripts.sh
@@ -19,3 +19,56 @@ echo ${JOB_SCRIPT} ${FASTQ} >> ${SWARM_FILENAME}
 done
 
 ##########
+
+SWARM_FILENAME=/data/ADRD/AD_multiregion/fastq_processing/swarmscripts/doubletdetection_swarm.swarm
+FASTQ_LIST=$(cat /data/ADRD/AD_multiregion/fastq_processing/round_1_fastqs.txt)
+JOB_SCRIPT=/data/ADRD/AD_multiregion/fastq_processing/jobscripts/run_demuxafy_doubletdetection_swarm.sh
+
+for FASTQ in ${FASTQ_LIST}
+do
+echo ${JOB_SCRIPT} ${FASTQ} >> ${SWARM_FILENAME}
+done
+
+##########
+
+SWARM_FILENAME=/data/ADRD/AD_multiregion/fastq_processing/swarmscripts/scdblfinder_swarm.swarm
+FASTQ_LIST=$(cat /data/ADRD/AD_multiregion/fastq_processing/round_1_fastqs.txt)
+JOB_SCRIPT=/data/ADRD/AD_multiregion/fastq_processing/jobscripts/run_demuxafy_scdblfinder_swarm.sh
+
+for FASTQ in ${FASTQ_LIST}
+do
+echo ${JOB_SCRIPT} ${FASTQ} >> ${SWARM_FILENAME}
+done
+
+##########
+
+SWARM_FILENAME=/data/ADRD/AD_multiregion/fastq_processing/swarmscripts/scds_swarm.swarm
+FASTQ_LIST=$(cat /data/ADRD/AD_multiregion/fastq_processing/round_1_fastqs.txt)
+JOB_SCRIPT=/data/ADRD/AD_multiregion/fastq_processing/jobscripts/run_demuxafy_scds_swarm.sh
+
+for FASTQ in ${FASTQ_LIST}
+do
+echo ${JOB_SCRIPT} ${FASTQ} >> ${SWARM_FILENAME}
+done
+
+##########
+
+SWARM_FILENAME=/data/ADRD/AD_multiregion/fastq_processing/swarmscripts/demuxafy_combine_results.swarm
+FASTQ_LIST=$(cat /data/ADRD/AD_multiregion/fastq_processing/round_1_fastqs.txt)
+JOB_SCRIPT=/data/ADRD/AD_multiregion/fastq_processing/jobscripts/demuxafy_combine_results.sh
+
+for FASTQ in ${FASTQ_LIST}
+do
+echo ${JOB_SCRIPT} ${FASTQ} >> ${SWARM_FILENAME}
+done
+
+##########
+
+SWARM_FILENAME=/data/ADRD/AD_multiregion/fastq_processing/swarmscripts/filter_doublets.swarm
+FASTQ_LIST=$(cat /data/ADRD/AD_multiregion/fastq_processing/round_1_fastqs.txt)
+JOB_SCRIPT=/data/ADRD/AD_multiregion/fastq_processing/jobscripts/filter_doublets.sh
+
+for FASTQ in ${FASTQ_LIST}
+do
+echo ${JOB_SCRIPT} ${FASTQ} >> ${SWARM_FILENAME}
+done
