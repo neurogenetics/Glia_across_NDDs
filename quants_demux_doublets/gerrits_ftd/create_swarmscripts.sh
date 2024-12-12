b8c223a6e056dd9aa22d0a341cf7f8c0061c7f09
Liam Horan-Portelance <142847439+lhoranportelance@users.noreply.github.com>
2024-12-12 13:57:23 -0500

    Create create_swarmscripts.sh

---
+++ quants_demux_doublets/gerrits_ftd/create_swarmscripts.sh
@@ -0,0 +1,85 @@
+SWARM_FILENAME=/data/ADRD/gerrits_ftd_snRNA/fastq_processing/swarmscripts/prefetch_fasterq_dump_swarm.swarm
+FASTQ_LIST=$(cat /data/ADRD/gerrits_ftd_snRNA/fastq_processing/gerrits_srrs.txt)
+JOB_SCRIPT=/data/ADRD/gerrits_ftd_snRNA/fastq_processing/jobscripts/prefetch_fasterq_dump_swarm.sh
+
+for FASTQ in ${FASTQ_LIST}
+do
+echo ${JOB_SCRIPT} ${FASTQ} >> ${SWARM_FILENAME}
+done
+
+##########
+
+SWARM_FILENAME=/data/ADRD/gerrits_ftd_snRNA/fastq_processing/swarmscripts/cellranger_swarm.swarm
+FASTQ_LIST=$(cat /data/ADRD/gerrits_ftd_snRNA/fastq_processing/gerrits_srrs.txt)
+JOB_SCRIPT=/data/ADRD/gerrits_ftd_snRNA/fastq_processing/jobscripts/run_cellranger_swarm.sh
+
+for FASTQ in ${FASTQ_LIST}
+do
+echo ${JOB_SCRIPT} ${FASTQ} >> ${SWARM_FILENAME}
+done
+
+##########
+
+SWARM_FILENAME=/data/ADRD/gerrits_ftd_snRNA/fastq_processing/swarmscripts/cellbender_swarm.swarm
+FASTQ_LIST=$(cat /data/ADRD/gerrits_ftd_snRNA/fastq_processing/gerrits_srrs.txt)
+JOB_SCRIPT=/data/ADRD/gerrits_ftd_snRNA/fastq_processing/jobscripts/run_cellbender_swarm.sh
+
+for FASTQ in ${FASTQ_LIST}
+do
+echo ${JOB_SCRIPT} ${FASTQ} >> ${SWARM_FILENAME}
+done
+
+##########
+
+SWARM_FILENAME=/data/ADRD/gerrits_ftd_snRNA/fastq_processing/swarmscripts/doubletdetection_swarm.swarm
+FASTQ_LIST=$(cat /data/ADRD/gerrits_ftd_snRNA/fastq_processing/gerrits_srrs.txt)
+JOB_SCRIPT=/data/ADRD/gerrits_ftd_snRNA/fastq_processing/jobscripts/run_demuxafy_doubletdetection_swarm.sh
+
+for FASTQ in ${FASTQ_LIST}
+do
+echo ${JOB_SCRIPT} ${FASTQ} >> ${SWARM_FILENAME}
+done
+
+##########
+
+SWARM_FILENAME=/data/ADRD/gerrits_ftd_snRNA/fastq_processing/swarmscripts/scdblfinder_swarm.swarm
+FASTQ_LIST=$(cat /data/ADRD/gerrits_ftd_snRNA/fastq_processing/gerrits_srrs.txt)
+JOB_SCRIPT=/data/ADRD/gerrits_ftd_snRNA/fastq_processing/jobscripts/run_demuxafy_scdblfinder_swarm.sh
+
+for FASTQ in ${FASTQ_LIST}
+do
+echo ${JOB_SCRIPT} ${FASTQ} >> ${SWARM_FILENAME}
+done
+
+##########
+
+SWARM_FILENAME=/data/ADRD/gerrits_ftd_snRNA/fastq_processing/swarmscripts/scds_swarm.swarm
+FASTQ_LIST=$(cat /data/ADRD/gerrits_ftd_snRNA/fastq_processing/gerrits_srrs.txt)
+JOB_SCRIPT=/data/ADRD/gerrits_ftd_snRNA/fastq_processing/jobscripts/run_demuxafy_scds_swarm.sh
+
+for FASTQ in ${FASTQ_LIST}
+do
+echo ${JOB_SCRIPT} ${FASTQ} >> ${SWARM_FILENAME}
+done
+
+##########
+
+SWARM_FILENAME=/data/ADRD/gerrits_ftd_snRNA/fastq_processing/swarmscripts/demuxafy_combine_results.swarm
+FASTQ_LIST=$(cat /data/ADRD/gerrits_ftd_snRNA/fastq_processing/gerrits_srrs.txt)
+JOB_SCRIPT=/data/ADRD/gerrits_ftd_snRNA/fastq_processing/jobscripts/demuxafy_combine_results.sh
+
+for FASTQ in ${FASTQ_LIST}
+do
+echo ${JOB_SCRIPT} ${FASTQ} >> ${SWARM_FILENAME}
+done
+
+##########
+
+SWARM_FILENAME=/data/ADRD/gerrits_ftd_snRNA/fastq_processing/swarmscripts/filter_doublets.swarm
+FASTQ_LIST=$(cat /data/ADRD/gerrits_ftd_snRNA/fastq_processing/gerrits_srrs.txt)
+JOB_SCRIPT=/data/ADRD/gerrits_ftd_snRNA/fastq_processing/jobscripts/filter_doublets.sh
+
+for FASTQ in ${FASTQ_LIST}
+do
+echo ${JOB_SCRIPT} ${FASTQ} >> ${SWARM_FILENAME}
+done
