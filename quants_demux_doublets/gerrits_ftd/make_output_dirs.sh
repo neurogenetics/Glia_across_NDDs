90ac4ad4758c6d772bfe1b4704b1e44d6f0e9215
Liam Horan-Portelance <142847439+lhoranportelance@users.noreply.github.com>
2024-12-12 13:44:06 -0500

    Create make_output_dirs.sh

---
+++ quants_demux_doublets/gerrits_ftd/make_output_dirs.sh
@@ -0,0 +1,20 @@
+dos2unix gerrits_srrs.txt
+
+FASTQ_LIST=$(cat /data/ADRD/gerrits_ftd_snRNA/fastq_processing/gerrits_srrs.txt)
+
+for FASTQ in ${FASTQ_LIST}
+do
+mkdir -p /data/ADRD/gerrits_ftd_snRNA/fastq_processing/swarmscripts
+mkdir -p /data/ADRD/gerrits_ftd_snRNA/fastq_processing/swarmlogs
+mkdir -p /data/ADRD/gerrits_ftd_snRNA/fastq_processing/jobscripts
+mkdir -p /data/ADRD/gerrits_ftd_snRNA/fastq_processing/all_fastqs/fastq/$FASTQ
+mkdir -p /data/ADRD/gerrits_ftd_snRNA/fastq_processing/all_fastqs/sra
+mkdir -p /data/ADRD/gerrits_ftd_snRNA/fastq_processing/cellranger/outs/$FASTQ
+mkdir -p /data/ADRD/gerrits_ftd_snRNA/fastq_processing/cellbender/outs/$FASTQ
+mkdir -p /data/ADRD/gerrits_ftd_snRNA/fastq_processing/demuxafy/outs/$FASTQ
+mkdir -p /data/ADRD/gerrits_ftd_snRNA/fastq_processing/demuxafy/outs/$FASTQ/doubletdetection
+mkdir -p /data/ADRD/gerrits_ftd_snRNA/fastq_processing/demuxafy/outs/$FASTQ/scdblfinder
+mkdir -p /data/ADRD/gerrits_ftd_snRNA/fastq_processing/demuxafy/outs/$FASTQ/scds
+mkdir -p /data/ADRD/gerrits_ftd_snRNA/fastq_processing/demuxafy/outs/$FASTQ/combined
+mkdir -p /data/ADRD/gerrits_ftd_snRNA/fastq_processing/final_outs/$FASTQ
+done
