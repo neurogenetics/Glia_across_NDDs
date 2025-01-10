8ff2a7c9c80ac530ac8b7f4d6153d5cb665f1126
Liam Horan-Portelance <142847439+lhoranportelance@users.noreply.github.com>
2025-01-10 11:19:30 -0500

    Update download_bams_aws.sh

--- quants_demux_doublets/gerrits_ftd/download_bams_aws.sh
+++ quants_demux_doublets/gerrits_ftd/download_bams_aws.sh
@@ -8,3 +8,13 @@ do
 aws --no-sign-request s3 cp s3://sra-pub-src-2/$FASTQ/ ./bams --recursive
 echo "$FASTQ done"
 done
+
+# for some reason some were in a different bucket, repeat w/ different bucket name
+
+FASTQ_LIST=$(cat /data/ADRD/gerrits_ftd_snRNA/fastq_processing/gerrits_srrs.txt)
+
+for FASTQ in ${FASTQ_LIST}
+do
+aws --no-sign-request s3 cp s3://sra-pub-src-1/$FASTQ/ ./bams --recursive
+echo "$FASTQ done"
+done
