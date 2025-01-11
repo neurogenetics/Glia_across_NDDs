031fcfc1261a585ad38e5fe06de9251c3470f415
Liam Horan-Portelance <142847439+lhoranportelance@users.noreply.github.com>
2025-01-11 10:52:16 -0500

    Update make_output_dirs.sh

--- quants_demux_doublets/gerrits_ftd/make_output_dirs.sh
+++ quants_demux_doublets/gerrits_ftd/make_output_dirs.sh
@@ -1,15 +1,15 @@
 dos2unix gerrits_srrs.txt
+dos2unix gerrits_samples.txt
 
-FASTQ_LIST=$(cat /data/ADRD/gerrits_ftd_snRNA/fastq_processing/gerrits_srrs.txt)
+FASTQ_LIST=$(cat /data/ADRD/gerrits_ftd_snRNA/fastq_processing/gerrits_samples.txt)
 
 for FASTQ in ${FASTQ_LIST}
 do
 mkdir -p /data/ADRD/gerrits_ftd_snRNA/fastq_processing/swarmscripts
-mkdir -p /data/ADRD/gerrits_ftd_snRNA/fastq_processing/bams
 mkdir -p /data/ADRD/gerrits_ftd_snRNA/fastq_processing/swarmlogs
 mkdir -p /data/ADRD/gerrits_ftd_snRNA/fastq_processing/jobscripts
-mkdir -p /data/ADRD/gerrits_ftd_snRNA/fastq_processing/all_fastqs/fastq/$FASTQ
-mkdir -p /data/ADRD/gerrits_ftd_snRNA/fastq_processing/all_fastqs/sra
+mkdir -p /data/ADRD/gerrits_ftd_snRNA/fastq_processing/bams
+mkdir -p /data/ADRD/gerrits_ftd_snRNA/fastq_processing/fastqs/$FASTQ
 mkdir -p /data/ADRD/gerrits_ftd_snRNA/fastq_processing/cellranger/outs/$FASTQ
 mkdir -p /data/ADRD/gerrits_ftd_snRNA/fastq_processing/cellbender/outs/$FASTQ
 mkdir -p /data/ADRD/gerrits_ftd_snRNA/fastq_processing/demuxafy/outs/$FASTQ
